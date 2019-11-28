b63018dd10e7ae97685deb1cc6c0e9a1c809ffd7
shabnamsadegh <sadegh.shabnam@gmail.com>
2019-11-28 10:39:44 +0100

    fix minor bugs

--- run_classifier_with_tfhub.py
+++ run_classifier_with_tfhub.py
@@ -144,7 +144,7 @@ def model_fn_builder(num_labels, learning_rate, num_train_steps,
     elif mode == tf.estimator.ModeKeys.PREDICT:
       print("This is a debugging message")
       output_spec = tf.contrib.tpu.TPUEstimatorSpec(
-          mode=mode, predictions={"probabilities": probabilities, "all_layers":  all_encoder_layers * probabilities.shape[0]}) # I added the bert_outputs for visualization of layers
+          mode=mode, predictions={"probabilities": probabilities, "all_layers":  [all_encoder_layers] * probabilities.shape[0]}) # I added the bert_outputs for visualization of layers
     else:
       raise ValueError(
           "Only TRAIN, EVAL and PREDICT modes are supported: %s" % (mode))
